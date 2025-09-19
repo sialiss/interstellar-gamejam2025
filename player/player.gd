69027158f57afcc4fdc8603bb401cffb53ea5026
och-och <och.hco.och.hco@gmail.com>
2025-09-19 18:52:18 +0300

    refactor player

--- player/player.gd
+++ player/player.gd
@@ -1,74 +1,69 @@
 extends CharacterBody3D
 
-@onready var ray: RayCast3D = $Camera3D/RayCast3D
 signal show_prompt(prompt_text)
 #signal show_timer_prompt(prompt_text)
 signal hide_prompt()
 
-var can_move: bool = true
-# Скорость движения
+@onready var ray: RayCast3D = %InteractionRay
+
+## Скорость движения
 @export var speed: float = 5.0
-# Чувствительность мыши
+## Чувствительность мыши
 @export var mouse_sensitivity: float = 0.002
-# Скорость прыжка
+## Скорость прыжка
 @export var JUMP_VELOCITY: float = 4.5
+
+var can_move: bool = true
 
 # Переменные для управления камерой
 var camera_angle: float = 0
 var do_camera_move: bool = true
 
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _process(_delta):
+func _input(event: InputEvent):
 	# Захват мышки (как в арксе)
 	if Input.is_action_just_pressed("ui_cancel"):
-		if Input.mouse_mode != Input.MOUSE_MODE_VISIBLE:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			do_camera_move = false
-		else:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-			do_camera_move = true
-
+		_toggle_mouse_capture()
+	
 	# Респавн
 	if Input.is_action_just_pressed("restart"):
 		position = Vector3(0, 10, 0)
-		do_camera_move = true
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-	# Подсказка при наведении на объект
-	if ray.is_colliding():
-		var obj = ray.get_collider()
-		if obj.is_in_group("interactable"):
-			emit_signal("show_prompt", "[E] - interact")
-		elif obj.is_in_group("npc"):
-			emit_signal("show_prompt", "[E] - talk")
-		else:
-			emit_signal("hide_prompt")
-	else:
-		emit_signal("hide_prompt")
-
-
-func _input(event):
+		_set_mouse_capture(true)
+	
 	if event is InputEventMouseMotion and do_camera_move:
 		# Вращение камеры по вертикали
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
 		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, -1.5, 1.5)
 
+func _process(_delta: float):
+	# Подсказка при наведении на объект
+	if ray.is_colliding():
+		var obj = ray.get_collider()
+		if obj.is_in_group("interactable"):
+			show_prompt.emit("[E] - interact")
+		elif obj.is_in_group("npc"):
+			show_prompt.emit("[E] - talk")
+		else:
+			hide_prompt.emit()
+	else:
+		hide_prompt.emit()
+
 func _physics_process(delta: float) -> void:
 	if not can_move:
 		return  # игрок заморожен
-
+	
 	# Рассчёт скорости в падении
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Прыжок
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-
+	
 	# Управление движением
 	var direction: Vector3 = Vector3.ZERO # todo: сейчас не сохраняет инерцию
 	if Input.is_action_pressed("move_forward"):
@@ -79,14 +74,26 @@ func _physics_process(delta: float) -> void:
 		direction -= transform.basis.x
 	if Input.is_action_pressed("move_right"):
 		direction += transform.basis.x
-
+	
 	# Нормализация направления и применение скорости
 	direction = direction.normalized()
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
-
+	
 	# Применение движения
 	move_and_slide()
 
 func _on_dialogue_mode(is_dialogue_mode: bool):
 	can_move = !is_dialogue_mode
+
+func _toggle_mouse_capture():
+	var should_capture := Input.mouse_mode == Input.MOUSE_MODE_VISIBLE
+	_set_mouse_capture(should_capture)
+
+func _set_mouse_capture(value: bool):
+	if value:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		do_camera_move = true
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		do_camera_move = false
