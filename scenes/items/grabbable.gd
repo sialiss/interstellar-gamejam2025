62276d3e252ccf55727578c089470da9ce172994
och-och <och.hco.och.hco@gmail.com>
2025-09-21 00:31:11 +0300

    move grabbables to different layer when grabbed

--- scenes/items/grabbable.gd
+++ scenes/items/grabbable.gd
@@ -1,7 +1,15 @@
 class_name Grabbable extends RigidBody3D
 
+const NORMAL_LAYER = 3
+const GRABBED_LAYER = 7
+
 func grabbed():
 	freeze = true
+	set_collision_layer_value(NORMAL_LAYER, false)
+	set_collision_layer_value(GRABBED_LAYER, true)
+
 
 func ungrabbed():
 	freeze = false
+	set_collision_layer_value(NORMAL_LAYER, true)
+	set_collision_layer_value(GRABBED_LAYER, false)
