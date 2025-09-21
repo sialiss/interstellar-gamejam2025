a515ec5f3669c5c6437cc2792fdf61877f1a483b
och-och <och.hco.och.hco@gmail.com>
2025-09-22 02:47:19 +0300

    pause menu 1

--- scenes/menu/main/main_menu.gd
+++ scenes/menu/main/main_menu.gd
@@ -1,15 +1,15 @@
 extends Node3D
 
 @onready var btn_start: Button = $CanvasLayer/Control/Start
-@onready var btn_credits: Button = $CanvasLayer/Control/Start
-@onready var btn_exit: Button = $CanvasLayer/Control/Start
+@onready var btn_credits: Button = $CanvasLayer/Control/Credits
+@onready var btn_exit: Button = $CanvasLayer/Control/Exit
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	
 	btn_start.pressed.connect(SceneManager.start_game)
 	#btn_credits.pressed.connect(SceneManager.open_credits)
-	btn_start.pressed.connect(get_tree().quit)
+	btn_exit.pressed.connect(get_tree().quit)
 	#TODO добавить скрытие кнопки выхода на вебе
 	#if ()
 		#btn_exit.visible = false
--- scenes/menu/rebirth/rebirth.gd
+++ scenes/menu/rebirth/rebirth.gd
@@ -1,6 +1,8 @@
 class_name RebirthMenu extends Node
 
 func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+
 	EventBus.reset_cycle()
 
 func restart_game():
