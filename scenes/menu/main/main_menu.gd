c97d34ab8ace7b0e25f23ae5f9b5714df728086d
Camellia <56206613+sialiss@users.noreply.github.com>
2025-09-22 20:41:34 +0300

    add credits and hide exit button on web

--- scenes/menu/main/main_menu.gd
+++ scenes/menu/main/main_menu.gd
@@ -1,15 +1,30 @@
 extends Node3D
 
-@onready var btn_start: Button = $CanvasLayer/Control/Start
-@onready var btn_credits: Button = $CanvasLayer/Control/Credits
-@onready var btn_exit: Button = $CanvasLayer/Control/Exit
+@onready var btn_start: Button = $CanvasLayer/Menu/Start
+@onready var btn_credits: Button = $CanvasLayer/Menu/Credits
+@onready var btn_exit: Button = $CanvasLayer/Menu/Exit
+@onready var btn_back: Button = $CanvasLayer/Credits/Back
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-
 	btn_start.pressed.connect(SceneManager.start_game)
-	#btn_credits.pressed.connect(SceneManager.open_credits)
+	btn_credits.pressed.connect(open_credits)
+	btn_back.pressed.connect(open_menu)
 	btn_exit.pressed.connect(get_tree().quit)
-	#TODO добавить скрытие кнопки выхода на вебе
-	#if ()
-		#btn_exit.visible = false
+	hide_credits()
+	if OS.get_name() == "HTML5":
+		$btn_exit.visible = false
+
+func open_menu():
+	$CanvasLayer/Menu.visible = true
+	hide_credits()
+
+func hide_menu():
+	$CanvasLayer/Menu.visible = false
+
+func open_credits():
+	$CanvasLayer/Credits.visible = true
+	hide_menu()
+
+func hide_credits():
+	$CanvasLayer/Credits.visible = false
